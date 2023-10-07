026f5e0396f0ff3ae8872deacd157f46a2556074
Sayuru Bopitiya <sayururehan77@gmail.com>
2023-10-07 23:56:33 +0530

    initial commit

---
+++ background.js
@@ -0,0 +1,37 @@
+chrome.alarms.onAlarm.addListener(
+    () => {
+        chrome.notifications.create(
+            // "drink_water",
+            {
+                type: "basic",
+                iconUrl: "images/icon.png",
+                title: "Stay Hydrated",
+                message: "How 'bout a cup of water human!?",
+                silent: false
+            },
+            () => { }
+        )
+    },
+)
+
+chrome.runtime.onMessage.addListener(
+    function (request, sender, sendResponse) {
+        console.log(request);
+        if (request.time)
+            createAlarm();
+
+        sendResponse(() => {
+            return false
+        });
+    }
+);
+
+function createAlarm() {
+    chrome.alarms.create(
+        "drink_water",
+        {
+            delayInMinutes: 1,
+            periodInMinutes: 1
+        }
+    );
+}
